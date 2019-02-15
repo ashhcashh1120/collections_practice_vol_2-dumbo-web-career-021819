67d8f82485d104379785e0ca106d685ad4b2ae25
Ashley Archibald <aarchibald20@gmail.com>
2019-02-15 06:55:41 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -25,8 +25,8 @@ end
 
 def count_elements(str)
   str.uniq.each {|i| x = 0
-         str.each {|i2| if i2 == i then x += 1 end}
-         i[:x] = x}
+         str.each {|i2| if i2 == i then count += 1 end}
+         i[:count] = count}
 end
 
 def merge_data()
